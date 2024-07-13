c79407a92fcce4c9c9c1a4f2063e33be54858d1e
khanloo <m.nadirkhanloo1380@gmail.com>
2024-07-13 21:47:30 +0330

    Style : header code

--- src/js/script.js
+++ src/js/script.js
@@ -4,17 +4,17 @@ const menuDropdown = document.getElementById("menu_dropdown");
 const iconDropdown = document.getElementById("icon_dropdown");
 const textDropdown = document.getElementById("text_btn_dropdown");
 const itemDropdown = document.querySelectorAll(".item");
-const bod = document.querySelector("#bg_back_menu");
+const bgBackMenu = document.querySelector("#bg_back_menu");
 
-bod.addEventListener("click", () => {
+bgBackMenu.addEventListener("click", () => {
   menuDropdown.classList.add("hidden");
   iconDropdown.classList.remove("-rotate-180");
-  bod.classList.add("hidden");
+  bgBackMenu.classList.add("hidden");
 });
 function showMenu() {
   menuDropdown.classList.toggle("hidden");
   iconDropdown.classList.toggle("-rotate-180");
-  bod.classList.toggle("hidden");
+  bgBackMenu.classList.toggle("hidden");
 }
 
 let num = 0;
